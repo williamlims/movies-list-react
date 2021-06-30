ae52902c7a587539eee41555eb5308b670852b4e
William Lima <williamlims@hotmail.com>
2021-06-30 15:30:49 -0300

    alterations

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -1,9 +1,11 @@
 export const Bottom = () => (
-    <div class="navbar navbar-expand-lg navbar-light bg-light">
-        <br />
-        <br />
-        <p class="text-center">Todos os direitos reservados 2021</p>
-        <br />
-        <br />
+    <div class="container">
+        <div class="navbar navbar-expand-lg navbar-light bg-light">
+            <br />
+            <br />
+            <p class="col-md-12">Todos os direitos reservados 2021</p>
+            <br />
+            <br />
+        </div>
     </div>
 )
